f68518bb95b9a067fe555f0abcaafc6828cdad48
Gianni nuzzo <kennuzzo@gmail.com>
2015-04-27 13:25:07 +0800

    rename example endpoint to examples, added post

--- app/controllers/api/v1/example/rest_api.rb
+++ app/controllers/api/v1/example/rest_api.rb
@@ -4,11 +4,12 @@ module API
 
       class RestApi < Grape::API
         include Grape::Kaminari
-        paginate per_page: 1, max_per_page: 1, offset: 1
 
-        namespace :example do
+        namespace :examples do
           desc 'This is a Get Example'
           oauth2 false
+          paginate per_page: 1, max_per_page: 1, offset: 1
+
           get '/' do
             dummy_data = {
               id: 1,
@@ -23,20 +24,20 @@ module API
             present dummy_data, with: API::Entities::DummyDataEntity
           end
 
-          desc 'This is a Get Example'
+          desc 'This is a POST Example'
           oauth2 false
-          get '/' do
-            dummy_data = {
-              id: 1,
-              first: "Francesco",
-              last:  "Guccini",
-              dob:   Date.new(1940,06,14),
-              location: 'Modena, Italy',
-              internal_id: "this will be hidden via an entity",
-              created_at: DateTime.now,
-              updated_at: DateTime.now,
+          params do
+            requires :name, type: String, desc: 'The required name field', documentation: {example: 'example name'}
+            optional :dob, type: Date, desc: 'Day of birth', documentation: {example: Date.today}
+            optional :location, type: String, desc: 'the description about the location', documentation: {example: "Mestre, Italy"}
+          end
+          post '/' do
+            received_data = {
+              fullname: permitted_params[:name],
+              dob: permitted_params[:dob],
+              location: permitted_params[:location]
             }
-            present dummy_data, with: API::Entities::DummyDataEntity
+            present received_data, with: API::Entities::DummyDataEntity
           end
 
 
